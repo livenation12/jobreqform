30897e49ca68d8f4eb50aefa3914a5e178215cd7
livenation12 <joshdionio21@gmail.com>
2022-11-30 23:55:51 -1200

    Update formclass.php

--- formclass.php
+++ formclass.php
@@ -1,8 +1,6 @@
 <?php
-
-
 class MyForm {	
-	private $server = "mysql:host=localhost;dbname=mystore";
+	private $server = "mysql:host=localhost;dbname=";
 	private $root = "root";
 	private $password = "";
 	private $options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC);
@@ -22,4 +20,31 @@ class MyForm {
 		public function closeConnection(){
 			$this->con = null;
 		}
+		public function userLogin(){
+
+			if (isset($_POST['submit'])) {
+			$account_id = $_POST['account_id'];
+			$password = $_POST['password'];
+
+
+			$conn = $this->openConnection();
+			$stmt = $conn->prepare("SELECT * FROM admin WHERE account_id = ? AND password = ?");
+			$stmt->execute([$account_id, $password]);
+			$admin = $stmt->fetch();
+			$adminNum = $stmt->rowCount();
+			if($adminNum > 0){
+				echo "Welcome". $admin['account_id'];
+			}else{
+				echo "Incorrect account_id nor password";
+			}
+		}
+	}
+		public function set_admindata($array){
+			if(isset($_SESSION)){
+				session_start();
+
+				$_SESSION['admindata'] = array()
+		}
+	}
 }
+?>
