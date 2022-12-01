7e99414f76a7739f472086e90d20db5049dbfbde
livenation12 <joshdionio21@gmail.com>
2022-12-01 01:00:35 -1200

    Create adminlogin.php
    dont know how to make a paging style

---
+++ adminlogin.php
@@ -0,0 +1,22 @@
+<?php
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>JobRequestAdmin</title>
+</head>
+<body>
+	<form action="" method="post">
+	<label>Username:</label>
+	<input type="username" name="username">
+	<label>Password:</label>
+	<input type="password" name="password">
+	<input type="submit" name="submit" value="Enter">
+	</form>
+</body>
+</html>
