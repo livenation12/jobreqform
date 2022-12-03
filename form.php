477f705bd369b655fef2fba2916da6112d83b088
livenation12 <joshdionio21@gmail.com>
2022-12-02 14:12:15 -1200

    user data insert to db
    concatinated fullname, updated request form

--- form.php
+++ form.php
@@ -1,39 +1,43 @@
-<?php?>
+<?php
+require_once('formclass.php');
+$class->userInsertData();
+
+?>
 
 
 
 <!DOCTYPE html>
 <html>
 <head>
+
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Service Request Form</title>
+	<title>Services Request Form</title>
 </head>
 <body>
-	<p>Request for <h5>IT SERVICES</h5></p>
-	<table border="1" width="300">
-		<div class= "row-form">
+<div>
+	<p>Request for <span style="font-size: 20px;">IT SERVICEsS</p>
+		<hr>
+	<table>
+		<div>
 		<tr>
 			<form action="" method="post">
 			<td>
 				<label>Requesting Dept/Office:</label>
-				<input type="text" name="deptname">
+				<input type="text" name="deptname" class="form-control">
 			</td>
 			<td>
 				<label>Dept/Office Account ID:</label>
-				<input type="text" name="deptid">
-			</td>
-			<td>
-				<label>Dept/Office Account ID:</label>
-				<input type="text" name="deptid">
-			</td>
+				<input type="text" name="deptid" class="form-control">
+			</td> 
 			<td>
 				<label>Contact/Local #:</label>
-				<input type="text" name="contact">
+				<input type="text" name="contact" class="form-control">
 			</td>
 			<td>
 				<label>Date:</label>
-				<input type="date" name="date">
+				<input type="date" name="date_sub" hidden>
 			</td>
 			
 		</tr>
@@ -42,11 +46,14 @@
 				<tr>
 					<td>
 						<label>Name of Dept/Office Head:</label>
-						<input type="text" name="deptheadname">
+						<input type="text" name="deptheadfname" class="form-control" placeholder="Firstname" required>
+						<input type="text" name="deptheadmidname" class="form-control" placeholder="Middlename/Optional">
+						<input type="text" name="deptheadlname" class="form-control" placeholder="Lastname" required>
+						<input type="text" name="deptheadsuffix" class="form-control" placeholder="Suffix/Optional">
 					</td>
 					<td>
 						<label>Signature of Requesting Dept/Office Head:</label>
-						<input type="file" name="signature">
+						<input type="text" name="dept_head_sign" placeholder="comply later" readonly class="form-control">
 					</td>
 				</tr>
 			</div>
@@ -54,125 +61,130 @@
 				<tr>
 					<td>
 						<label>Name of End User:</label>
-						<input type="text" name="eusername">
+						<input type="text" name="euserfname" class="form-control" placeholder="Firstname" required>
+						<input type="text" name="eusermidname" class="form-control" placeholder="Middlename" >
+						<input type="text" name="euserlname" class="form-control" placeholder="Lastname" required>
+						<input type="text" name="eusersuffix" class="form-control" placeholder="Suffix/Optional">
 					</td>
 					<td>
 						<label>Position:</label>
-						<input type="text" name="position">
+						<input type="text" name="position" class="form-control" required>
 					</td>
 				</tr>
 				<tr>	
 					<td>
 						<label>Equipment Type/Description:</label>
-						<input type="text" name="equiptype">
+						<input type="text" name="equip_type" class="form-control" required>
 					</td>
 					<td>
 						<label>Equipment Serial Number:</label>
-						<input type="text" name="equiptype">
+						<input type="text" name="equip_number" class="form-control" required>
 					</td>
-				</tr>
+				
 			</div>	
 	</table>
+	
 <hr>
+
 	<table>
 		<div class="container">
 		
 			<tr>
 					<p>Equipment Issue/s: <i>(Check all that apply)</i></p>
 				<td>
-					<input type="checkbox" name="issues[]">
+					<input type="checkbox" name="issues[]" value="Application crash or OS blue screen">
 					<label>Application crash or OS blue screen</label>
 				</td>
 				<td>
-					<input type="checkbox" name="issues[]">
+					<input type="checkbox" name="issues[]" value="Application crash or OS blue screen">
 					<label>Application crash or OS blue screen</label>
 				</td>
 				<td>
-					<input type="checkbox" name="issues[]">
+					<input type="checkbox" name="issues[]" value="Damaged motherboard">
 					<label>Damaged motherboard</label>
 				</td>		
 			</tr>	 
 			<tr>
 				<td>
-					<input type="checkbox" name="issues[]">
+					<input type="checkbox" name="issues[]" value="Application won't operate">
 					<label>Application won't operate</label>
 				</td>
 				<td>
-					<input type="checkbox" name="issues[]">
+					<input type="checkbox" name="issues[]" value="No display">
 					<label>No display</label>
 				</td>
 				<td>
-					<input type="checkbox" name="issues[]">
+					<input type="checkbox" name="issues[]" value="Damaged Hard drive">
 					<label>Damaged Hard drive</label>
 				</td>	
 			</tr>
 			<tr>
 				<td>
-					<input type="checkbox" name="issues[]">
-					<label>Unserviceable</label>
+					<input type="checkbox" name="issues[]" value="Unservicesable">
+					<label>Unservicesable</label>
 				</td>
 				<td>
-					<input type="checkbox" name="issues[]">
+					<input type="checkbox" name="issues[]" value="Printer bunking">
 					<label>Printer bunking</label>
 				</td>
 				<td>
-					<input type="checkbox" name="issues[]">
+					<input type="checkbox" name="issues[]" value="Damaged memory">
 					<label>Damaged memory</label>
 				</td>
 			</tr>
 			<tr>
 				<td>
-					<input type="checkbox" name="issues[]">
+					<input type="checkbox" name="issues[]" value="Equipment won't boot or power up">
 					<label>Equipment won't boot or power up</label>
 				</td>
 				<td>
-					<input type="checkbox" name="issues[]">
+					<input type="checkbox" name="issues[]" value="Equipment shuts down or reboots">
 					<label>Equipment shuts down or reboots</label>
 				</td>
 				<td>
-					<input type="checkbox" name="issues[]">
+					<input type="checkbox" name="issues[]" value="Display issue">
 					<label>Display issue</label>
 				</td>
 			</tr>
 			<tr>
 				<td>
-					<input type="checkbox" name="issues[]">
+					<input type="checkbox" name="issues[]" value="Can't access the internet">
 					<label>Can't access the internet</label>
 				</td>
 				<td>
-					<input type="checkbox" name="issues[]">
+					<input type="checkbox" name="issues[]" value="Virus or malware">
 					<label>Virus or malware</label>
 				</td>
 				<td>
-					<input type="checkbox" name="issues[]">
+					<input type="checkbox" name="issues[]" value="Equipment is slow">
 					<label>Equipment is slow</label>
 				</td>
 			</tr>
 			<tr>
 					<td>
-					<input type="checkbox" name="issues[]">
+					<input type="checkbox" name="issues[]" value="Won't print">
 					<label>Won't print</label>
 				</td>
 				<td>
-					<input type="checkbox" name="issues[]">
+					<input type="checkbox" name="issues[]" value="No internet connection">
 					<label>No internet connection</label>
 				</td>
 				<td>
-					<input type="checkbox" name="issues[]">
+					<input type="checkbox" name="issues[]" value="Handset no dial tone">
 					<label>Handset no dial tone</label>
 				</td>
 			</tr>
 			<tr>
 				<td>
-					<input type="checkbox" name="issues[]">
+					<input type="checkbox" name="issues[]" value="Application won't open">
 					<label>Application won't open</label>
 				</td>
 				<td>
-					<input type="checkbox" name="issues[]">
+					<input type="checkbox" name="issues[]" value="Installation">
 					<label>Installation(OS, Apps, Internet)</label>
 				</td>
 				<td>
-					<input type="checkbox" name="issues[]">
+					<input type="checkbox" name="issues[]" value="Inspection">
 					<label>Inspection</label>
 				</td>
 			</tr>
@@ -185,68 +197,90 @@
 				</tr>
 			<tr>
 				<td>
-				<p>Required Service: <i>(Check all that apply)</i></p>
+				<p>Required Services: <i>(Check all that apply)</i></p>
 				</td>
 			</tr>
 			<tr>
 				<td>
+					<input type="checkbox" name="services[]" value="Diagnostic">
 					<label>Diagnostic</label>
-					<input type="text" name="service[]">
 				</td>
 				<td>
+					<input type="checkbox" name="services[]" value="Computer repair">
 					<label>Computer repair</label>
-					<input type="text" name="service[]">
+					
 				</td>
 				<td>
+					<input type="checkbox" name="services[]" value="Printer setup">
 					<label>Printer setup</label>
-					<input type="text" name="service[]">
+					
 				</td>
 			</tr>
 			<tr>
 				<td>
+					<input type="checkbox" name="services[]" value="Computer Format">
 					<label>Computer Format</label>
-					<input type="text" name="service[]">
+					
 				</td>
 				<td>
+					<input type="checkbox" name="services[]" value="Change hardware">
 					<label>Change hardware</label>
-					<input type="text" name="service[]">
+					
 				</td>
 				<td>
+					<input type="checkbox" name="services[]" value="Printer reset">
 					<label>Printer reset</label>
-					<input type="text" name="service[]">
+					
 				</td>
 			</tr>
 			<tr>
 			<td>
 					<label>Data recovery</label>
-					<input type="text" name="service[]">
+					<input type="checkbox" name="services[]" value="Data recovery">
 				</td>
 				<td>
+					<input type="checkbox" name="services[]" value="Computer upgrade">
 					<label>Computer upgrade</label>
-					<input type="text" name="service[]">
+					
 				</td>
 				<td>
+					<input type="checkbox" name="services[]" value="Router setup">
 					<label>Router setup</label>
-					<input type="text" name="service[]">
+					
 				</td>
 			</tr>
 			<tr>
 				<td>
+					<input type="checkbox" name="services[]" value="Virus/ Malware removal">
 					<label>Virus/ Malware removal</label>
-					<input type="text" name="service[]">
+					
 				</td>
 				<td>
+					<input type="checkbox" name="services[]" value="Printer repair">
 					<label>Printer repair</label>
-					<input type="text" name="service[]">
+					
 				</td>
 				<td>
+					<input type="checkbox" name="services[]" value="Router reset">
 					<label>Router reset</label>
-					<input type="text" name="service[]">
+					
 				</td>
 			</tr>
-			</form>
+			<tr></tr>
+			<tr></tr>
+			<tr>
+
+					<td> 
+						<input type="submit" name="submit">
+		</form>
+					</td>
+			</tr>
+				
+			
+			
 		</div>
 	</table>
+</div>
 
 </body>
 </html>
--- formclass.php
+++ formclass.php
@@ -1,10 +1,11 @@
 <?php
-class MyForm {	
+
+
+class RequestForm {	
 	private $server = "mysql:host=localhost;dbname=requestform";
 	private $root = "root";
 	private $password = "";
-	private $options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-	 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC);
+	private $options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC);
 	protected $con;
 
 //make a public function for openConnection with try and catch/this loop/function
@@ -18,10 +19,10 @@ class MyForm {
 					echo "There is something wrong". $e->getMessage();
 					}
 		}
-		public function closeConnection(){
+		public function closeConnection(){	
 			$this->con = null;
 		}
-		public function userLogin(){
+		public function adminLogin(){
 
 			if (isset($_POST['submit'])) {
 			$account_id = $_POST['account_id'];
@@ -35,49 +36,75 @@ class MyForm {
 			$adminNum = $stmt->rowCount();
 			if($adminNum > 0){
 				?>
-				<script>
-					alert("<?php echo "Welcome". $admin['account_id'];?>");
-					window.location.href = "form.php";
-				</script>
-				<?php
+		 	<script>
+	 			<?php
 			}else{
 				echo "Incorrect account_id nor password";
 			}
 		}
-	}
-		// public function set_admindata($array){
-		// 	if(isset($_SESSION)){
-		// 		session_start();
+	}	
+		public function set_admindata($array){
+			if(isset($_SESSION)){
+				session_start();
 
-		// 		$_SESSION['admindata'] = array();
-	// 	}
-	// }
-
-
-		public function userData(){
+				$_SESSION['admindata'] = array();
+			}
+		}
+		public function userInsertData(){
 			if (isset($_POST['submit'])) {
 				$req_dept = $_POST['deptname'];
 				$dept_acc_id = $_POST['deptid'];
 				$contact = $_POST['contact'];
-				$date_sub = $_POST['date_sub'];
-				$dept_head_name = $_POST['deptheadname'];
-				$dept_head_sign = $_POST['dept_head_sign'];
-				$euser_name = $_POST['eusername'];
+				// $date_sub = $_POST['date_sub'];
+
+
+				$dept_head_fname = $_POST['deptheadfname'];
+				$dept_head_midname = $_POST['deptheadmidname'];
+				$dept_head_suffix = $_POST['deptheadsuffix'];
+				$dept_head_lname = $_POST['deptheadlname'];
+				$dept_head_fullname = $dept_head_fname. $dept_head_midname. $dept_head_lname. $dept_head_suffix;
+
+				$euser_fname = $_POST['euserfname'];
+				$euser_midname = $_POST['eusermidname'];
+				$euser_lname = $_POST['euserlname'];
+				$euser_suffix = $_POST['eusersuffix'];
+				$euser_fullname = $euser_fname. " ". $euser_midname. " ". $euser_lname. " ". $euser_suffix;
+
+
 				$position = $_POST['position'];
 				$equip_type = $_POST['equip_type'];
-				$equip_num = $_POST['equip_num'];
-
+				$equip_num = $_POST['equip_number'];
+				$equip_issues= implode(',', $_POST['issues']);
+				$required_services = implode(',', $_POST['services']);
 				$conn = $this->openConnection();
-				$stmt = $conn->prepare("INSERT INTO users(req_dept, dept_acc_id, contact, date_sub, dept_head_name, dept_head_sign, euser_name, position, equip_type, equip_num)
-				VALUES(?,?,?,?,?,?,?,?,?,?)");
-				$stmt->execute([$req_dept, $dept_acc_id, $contact, $date_sub, $dept_head_name, $dept_head_sign, $euser_name, $position, $equip_type, $equip_num ]);
+				$stmt = $conn->prepare("INSERT INTO users(req_dept, dept_acc_id, contact, dept_head_fullname, euser_fullname, position, equip_type, equip_num, equip_issues, required_services)
+					VALUES(?,?,?,?,?,?,?,?,?,?)");
+				$stmt->execute([$req_dept, $dept_acc_id, $contact, $dept_head_fullname, $euser_fullname,
+					 $position, $equip_type, $equip_num, $equip_issues, $required_services]);
 				if($stmt == TRUE){
-					echo "successfully submitted!";
-				
-			}else{
-				echo "something is wrong";
+				echo "added";
 			}
 		}
 	}
-}
-?>
+	// }
+	// 	public function set_userdata($array){ 
+	// 		if (!isset($_SESSION)) {
+	// 			session_start();
+	// 		}
+
+	// 			$_SESSION['userdata'] = array(
+	// 			"fullname" => $array['firstname']. $array['lastname']. "",
+	// 		 	"access" => $array['access']);
+	// 			return $_SESSION['userdata'];  
+	// 	}
+
+	// 	public function get_userdata(){
+
+	// 		if (!isset($_SESSION)) {
+	// 			session_start();
+	// 	}
+	// 	return $_SESSION['userdata'];
+	// }
+	}
+
+$class = new RequestForm();
